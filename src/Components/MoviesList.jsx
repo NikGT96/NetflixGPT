79c5313d7fa24f95ffb2a33e316d162bfaac83c3
NikGT96 <nikhilgautum@gmail.com>
2025-04-06 15:00:27 +0530

    Refined logic in Secondary container, MoviesList and MoviesCard

--- src/Components/MoviesList.jsx
+++ src/Components/MoviesList.jsx
@@ -1,15 +1,16 @@
 import MoviesCard from "./MoviesCard";
 
-const MoviesList = ({Movies}) => {
-    return(
-        <div className="overflow-x-scroll">
-            <div className="flex gap-9 w-800">
-                {Movies?.map((movie) => (
-                    <MoviesCard key={movie.id} poster={movie.poster_path} />
-                    ))}
-            </div>
-        </div>
-    )
-}
+const MoviesList = ({ title, movies }) => {
+  return (
+    <div className="overflow-x-scroll p-6">
+      <h1 className="font-bold text-2xl text-white pb-6">{title}</h1>
+      <div className="flex gap-9 w-800">
+        {movies?.map((movie) => (
+          <MoviesCard key={movie.id} poster={movie.poster_path} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default MoviesList;
--- src/Components/SecondaryContainer.jsx
+++ src/Components/SecondaryContainer.jsx
@@ -6,34 +6,45 @@ const SecondaryContainer = () => {
   const nowPlayingMovies = useSelector(
     (store) => store?.movies?.nowPlayingMovies
   );
-  const trending = useSelector((store) => store.movies.trending);
-  console.log(trending);
+  const trending = useSelector((store) => store?.movies?.trending);
+
   return (
     <div className="bg-black -mt-12">
       <div className="pl-8 text-stone-100 relative -top-32">
-        <div className="p-6">
-          <h1 className="font-bold text-2xl pb-6">Now Playing Movies</h1>
-          <MoviesList Movies={nowPlayingMovies} />
-        </div>
-        <div className="p-6">
-          <h1 className="font-bold text-2xl pb-6">Trending</h1>
-          <MoviesList Movies={trending} />
-        </div>
-        <div className="p-6">
-          <h1 className="font-bold text-2xl pb-6">Latest</h1>
-          <MoviesList Movies={nowPlayingMovies} />
-        </div>
-        <div className="p-6">
-          <h1 className="font-bold text-2xl pb-6">Action</h1>
-          <MoviesList Movies={nowPlayingMovies} />
-        </div>
-        <div className="p-6">
-          <h1 className="font-bold text-2xl pb-6">Romantic</h1>
-          <MoviesList Movies={nowPlayingMovies} />
-        </div>
+        <MoviesList title={"Now Playing"} movies={nowPlayingMovies} />
+        <MoviesList title={"Trending"} movies={trending} />
+        <MoviesList title={"Romantic"} movies={nowPlayingMovies} />
+        <MoviesList title={"UpComing"} movies={nowPlayingMovies} />
       </div>
     </div>
   );
 };
 
 export default SecondaryContainer;
+
+// return (
+//   <div className="bg-black -mt-12">
+//     <div className="pl-8 text-stone-100 relative -top-32">
+//       <div className="p-6">
+//         <h1 className="font-bold text-2xl pb-6">Now Playing Movies</h1>
+//         <MoviesList Movies={nowPlayingMovies} />
+//       </div>
+//       <div className="p-6">
+//         <h1 className="font-bold text-2xl pb-6">Trending</h1>
+//         <MoviesList Movies={trending} />
+//       </div>
+//       <div className="p-6">
+//         <h1 className="font-bold text-2xl pb-6">Latest</h1>
+//         <MoviesList Movies={nowPlayingMovies} />
+//       </div>
+//       <div className="p-6">
+//         <h1 className="font-bold text-2xl pb-6">Action</h1>
+//         <MoviesList Movies={nowPlayingMovies} />
+//       </div>
+//       <div className="p-6">
+//         <h1 className="font-bold text-2xl pb-6">Romantic</h1>
+//         <MoviesList Movies={nowPlayingMovies} />
+//       </div>
+//     </div>
+//   </div>
+// );
